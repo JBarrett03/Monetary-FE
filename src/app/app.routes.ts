727b557898e39433865583bf34a26dae28080362
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-08 19:12:01 +0000

    Fixing routes navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,87 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { Test } from './components/test/test';
 import { UserDetails } from './components/user-details/user-details';
+import { Login } from './components/login/login';
+import { CreateAccount } from './components/create-account/create-account';
+import { Accounts } from './components/accounts/accounts';
+import { AccountDetails } from './components/account-details/account-details';
+import { TransactionDetails } from './components/transaction-details/transaction-details';
+import { Payments } from './components/payments/payments';
+import { Spending } from './components/spending/spending';
+import { ManageAccounts } from './components/manage-accounts/manage-accounts';
 
 /**
- * The routes array defines the routing configuration for the Angular application. It specifies the paths and corresponding components that should be rendered when a user navigates to a specific route. The routes include a default route for the home component, a route for displaying a list of users, and a route for displaying details of a specific user based on the user ID.
+ * Routes for the application.
  */
 export const routes: Routes = [
-
     /**
-     * The default route ('') renders the Home component when the user navigates to the root URL of the application.
+     * Home route.
      */
     {
         path: '',
         component: Home
     },
-
     /**
-     * The 'users' route renders the Test component when the user navigates to '/users'. This route is responsible for displaying a list of users with pagination functionality.
+     * Login route.
      */
     {
-        path: 'users',
-        component: Test
+        path: 'login',
+        component: Login
     },
-
     /**
-     * The 'users/:id' route renders the UserDetails component when the user navigates to '/users/:id', where ':id' is a placeholder for the user ID. This route is responsible for displaying the details of a specific user based on the provided user ID in the URL.
+     * Create account route.
      */
     {
-        path: 'users/:id',
+        path: 'create-account',
+        component: CreateAccount
+    },
+    /**
+     * Accounts route.
+     */
+    {
+        path: 'accounts',
+        component: Accounts
+    },
+    /**
+     * Account details route.
+     */
+    {
+        path: 'accounts/:accountId',
+        component: AccountDetails
+    },
+    /**
+     * Transaction details route.
+     */
+    {
+        path: 'accounts/:accountId/transactions/:transactionId',
+        component: TransactionDetails
+    },
+    /**
+     * User details route.
+     */
+    {
+        path: 'user-details/:userId',
         component: UserDetails
+    },
+    /**
+     * Payments route.
+     */
+    {
+        path: 'payments',
+        component: Payments
+    },
+    /**
+     * Spending route.
+     */
+    {
+        path: 'spending',
+        component: Spending
+    },
+    /**
+     * Manage accounts route.
+     */
+    {
+        path: 'manage-accounts',
+        component: ManageAccounts
     }
 ];
