799cb2a4ffea90b47300c1de71a431b734798360
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-08 18:35:11 +0000

    Return all user accounts

--- src/app/components/accounts/accounts.ts
+++ src/app/components/accounts/accounts.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AccountService } from '../../services/account-service';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 
 /**
  * The Accounts component is responsible for displaying the account-related information and functionality within the application. It serves as a placeholder for the accounts section of the application, where users can view and manage their account details.
  */
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './accounts.html',
   styleUrl: './accounts.css',
 })
@@ -13,6 +16,42 @@ import { Component } from '@angular/core';
 /**
  * The Accounts class defines the component logic for the accounts section of the application. Currently, it does not contain any specific functionality or properties, but it serves as a foundation for future development of account-related features and interactions within the application.
  */
-export class Accounts {
+export class Accounts implements OnInit {
+
+  /**
+   * List of accounts
+   */
+  accounts_list: any[] = [];
+
+  /**
+   * The constructor injects the AccountService for managing account-related operations, ChangeDetectorRef for manually triggering change detection, and Router for navigation purposes. These dependencies are essential for implementing the functionality of the accounts section and ensuring that the component can interact with the account data and navigate to other parts of the application as needed.
+   * @param accountService - An instance of the AccountService for managing account-related operations.
+   * @param cdr - ChangeDetectorRef for manually triggering change detection.
+   * @param router - Router for navigation purposes.
+   */
+  constructor(private accountService: AccountService, private cdr: ChangeDetectorRef, private router: Router) { }
+
+  /**
+ * Initializes the component and loads the list of accounts for the user.
+ * @returns void
+ */
+  ngOnInit() {
+    const userId = sessionStorage.getItem('userId');
+
+    if (!userId) {
+      return;
+    }
+
+    this.accountService.getAccounts(userId).subscribe({
+      next: (accounts) => {
+        this.accounts_list = accounts;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Failed to load accounts', err);
+        this.cdr.detectChanges();
+      }
+    });
+  }
 
 }
