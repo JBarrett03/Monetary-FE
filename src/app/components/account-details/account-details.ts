88a4cb30524bf44604b27971e18c1ddda9d09f50
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-08 17:37:41 +0000

    Retrieving user accounts

--- src/app/components/account-details/account-details.ts
+++ src/app/components/account-details/account-details.ts
@@ -1,18 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AccountService } from '../../services/account-service';
 
 /**
- * The AccountDetails component is responsible for displaying the details of a specific account within the application. It serves as a placeholder for the account details section, where users can view and manage the information related to their accounts.
+ * AccountDetails component
  */
 @Component({
+  standalone: true,
   selector: 'app-account-details',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './account-details.html',
   styleUrl: './account-details.css',
 })
 
 /**
- * The AccountDetails class defines the component logic for the account details section of the application. Currently, it does not contain any specific functionality or properties, but it serves as a foundation for future development of account-related features and interactions within the application.
+ * AccountDetails class
  */
-export class AccountDetails {
+export class AccountDetails implements OnInit {
 
+  /**
+   * Account data
+   */
+  account: any | null = null;
+  /**
+   * Error message
+   */
+  error: string | null = null;
+
+  /**
+   * Creates an instance of AccountDetails component.
+   * @param route ActivatedRoute for accessing route parameters.
+   * @param router Router for navigation.
+   * @param accountService Service for account operations.
+   * @param cdr Change detector reference.
+   */
+  constructor(private route: ActivatedRoute, private router: Router, private accountService: AccountService, private cdr: ChangeDetectorRef) { }
+
+  /**
+   * Initializes the component and loads the account details and transactions.
+   * @returns void
+   */
+  ngOnInit() {
+    const userId = sessionStorage.getItem('userId');
+    const accountId = this.route.snapshot.paramMap.get('accountId');
+
+    if (!userId || !accountId) {
+      this.error = 'Invalid user or account ID';
+      return;
+    }
+
+    sessionStorage.setItem('accountId', accountId);
+
+    this.accountService.getAccount(userId, accountId).subscribe({
+      next: (account) => {
+        this.account = account;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.error = 'Account not found';
+        this.cdr.detectChanges();
+      }
+    });
+  }
 }
