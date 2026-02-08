beddbd5c885abd2ba53fdfe17c8c83cf297421eb
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-08 13:41:20 +0000

    Adding auth user

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Authbutton } from '../authbutton/authbutton';
+import { Authuser } from '../authuser/authuser';
 
 @Component({
   selector: 'app-home',
-  imports: [Authbutton],
+  imports: [Authbutton, Authuser],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
