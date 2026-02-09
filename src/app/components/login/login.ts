da0ee367c721ea92e7f2d4eb0b7703bf3bfe7aeb
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-09 19:12:14 +0000

    Fixing login logic

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -51,7 +51,8 @@ export class Login {
       password: this.password
     }).subscribe({
       next: (res) => {
-        sessionStorage.setItem('userId', res.id);
+        sessionStorage.setItem('userId', res.userId);
+        sessionStorage.setItem('token', res.token);
         this.router.navigate(['/']);
       },
       error: (err) => {
