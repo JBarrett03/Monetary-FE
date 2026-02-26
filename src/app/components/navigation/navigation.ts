d2496459f560160681cb7b313d91523f6818b3e3
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-02-26 14:50:21 +0000

    Fixing login logic

--- src/app/components/navigation/navigation.ts
+++ src/app/components/navigation/navigation.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -8,12 +8,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navigation.html',
   styleUrl: './navigation.css',
 })
-export class Navigation {
-  get isLoggedIn(): boolean {
-    return !!sessionStorage.getItem('userId');
+export class Navigation implements OnInit {
+
+  ngOnInit() {}
+
+  get userId(): string | null {
+    return sessionStorage.getItem('userId');
   }
 
-  getUserId(): string | null {
-    return sessionStorage.getItem('userId');
+  get isLoggedIn(): boolean {
+    return !!this.userId;
   }
 }
