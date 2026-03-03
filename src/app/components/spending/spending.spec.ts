00dff621dda4027051edb5e8dae8be770d91d1b8
Joshua Barrett <barrett-j7@ulster.ac.uk>
2026-03-03 13:32:23 +0000

    Added unit tests for spending component

--- src/app/components/spending/spending.spec.ts
+++ src/app/components/spending/spending.spec.ts
@@ -1,20 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AccountService } from '../../services/account-service';
+import { TransactionService } from '../../services/transaction-service';
+import { of } from 'rxjs';
+import { vi } from 'vitest';
 import { Spending } from './spending';
 
 describe('Spending', () => {
   let component: Spending;
   let fixture: ComponentFixture<Spending>;
+  let accountService: any;
+  let transactionService: any;
 
   beforeEach(async () => {
+    accountService = {
+      getAccounts: vi.fn(),
+    };
+    transactionService = {
+      getTransactionSummary: vi.fn(),
+      getAccountTransactionSummary: vi.fn(),
+      getCategorySummary: vi.fn(),
+    };
     await TestBed.configureTestingModule({
-      imports: [Spending]
-    })
-    .compileComponents();
+      imports: [Spending],
+      providers: [
+        { provide: AccountService, useValue: accountService },
+        { provide: TransactionService, useValue: transactionService },
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Spending);
     component = fixture.componentInstance;
-    await fixture.whenStable();
   });
 
   it('should create', () => {
